c0db73ba21217298a1af4778c53b5b2c139d8e35
My Pham <my.pham@team.enouvo.com>
2022-08-06 15:22:37 +0700

    push code

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 import postApi from './api/postApi'
-import { setTextContent, truncateText } from './utils'
+import { getUlPagination, setTextContent, truncateText } from './utils'
 import dayjs from 'dayjs'
 import relativeTime from 'dayjs/plugin/relativeTime'
+import debounce from 'lodash.debounce'
 dayjs.extend(relativeTime)
 
 function createPostElement(post) {
@@ -38,15 +39,38 @@ function renderPostList(postList) {
   const ulElement = document.getElementById('postList')
   if (!ulElement) return
 
+  // clear curent list
+  ulElement.textContent = ''
+
   postList.forEach((post) => {
     const liElment = createPostElement(post)
     ulElement.appendChild(liElment)
   })
 }
 
+function renderPagination(pagination) {
+  const ulPagination = getUlPagination()
+  if (!pagination || !ulPagination) return
+
+  //calc totalPages
+  const { _page, _limit, _totalRows } = pagination
+  const totalPages = Math.ceil(_totalRows / _limit)
+
+  //seve page and totalPages to ulPagination
+  ulPagination.dataset.page = _page
+  ulPagination.dataset.totalPages = totalPages
+
+  // check if enable / disabled prev  links
+  if (_page <= 1) ulPagination.firstElementChild?.classList.add('disabled')
+  else ulPagination.firstElementChild?.classList.remove('disabled')
+  // check if enable / disabled  next links
+  if (_page >= totalPages) ulPagination.lastElementChild?.classList.add('disabled')
+  else ulPagination.lastElementChild?.classList.remove('disabled')
+}
+
 function initPagination() {
   //bind click event for prev/next link ( ràng buộc sự kiện nhấp chuột cho liên kết prev/next link )
-  const ulPagination = document.getElementById('pagination')
+  const ulPagination = getUlPagination()
   if (!ulPagination) return
 
   // add click event for prev link (thêm sự kiện nhấp chuột cho prev )
@@ -64,21 +88,40 @@ function initPagination() {
 
 function handlePrevClick(e) {
   e.preventDefault()
-  console.log('prev')
+
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+
+  const page = Number.parseInt(ulPagination.dataset.page) || 1
+  if (page <= 1) return
+
+  handleFilterChange('_page', page - 1)
 }
 
 function handleNextClick(e) {
   e.preventDefault()
-  console.log('next')
+  const ulPagination = getUlPagination()
+  if (!ulPagination) return
+
+  const page = Number.parseInt(ulPagination.dataset.page) || 1
+  const totalPages = ulPagination.dataset.totalPages
+  if (page >= totalPages) return
+
+  handleFilterChange('_page', page + 1)
 }
 
-function handleFilterChange(filterName, filterValue) {
-  const url = new URL(window.location)
-  url.searchParams.set(filterName, filterValue)
-  history.pushState({}, '', url)
+async function handleFilterChange(filterName, filterValue) {
+  try {
+    const url = new URL(window.location)
+    url.searchParams.set(filterName, filterValue)
+    history.pushState({}, '', url)
 
-  // gọi lại API
-  // render post list
+    // gọi lại API
+    // render post list
+    const { data, pagination } = await postApi.getAll(url.searchParams)
+    renderPostList(data)
+    renderPagination(pagination)
+  } catch (error) {}
 }
 
 function initURL() {
@@ -90,15 +133,33 @@ function initURL() {
 
   history.pushState({}, '', url)
 }
+
+function initSearch() {
+  const searchInput = document.getElementById('searchInput')
+  if (!searchInput) return
+
+  // set Default values from query params (đặt các giá trị Mặc định từ các tham số truy vấn)
+  const debounceSearch = debounce(
+    (event) => handleFilterChange('title_like', event.target.value),
+    500
+  )
+  searchInput.addEventListener('input', debounceSearch)
+}
+
 ;(async () => {
   try {
+    // attach click event for links (đính kèm sự kiện nhấp chuột cho các liên kết)
     initPagination()
+    initSearch()
+
+    // set default pagination
     initURL()
 
+    // render post list based URL params
     const queryParams = new URLSearchParams(window.location.search)
-    console.log(new URLSearchParams(window.location.search))
     const { data, pagination } = await postApi.getAll(queryParams)
     renderPostList(data)
+    renderPagination(pagination)
   } catch (error) {
     console.log('get all failed', error)
   }
